3f8e5648606fb5cb5e6af76f03e1dbd78254dd41
olivier-bernard <olivier_bernard@ymail.com>
2025-10-15 09:02:20 +0000

    updating style setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,23 @@ const config: Config = {
   theme: {
     extend: {},
   },
-
+  daisyui: {
+    themes: [
+      "light", // Default light theme
+      "dark",  // Default dark theme
+      {
+        mytheme: {
+          primary: "#4CAF50", // Green for buttons and links
+          secondary: "#FF9800", // Orange for accents
+          accent: "#03A9F4", // Blue for highlights
+          neutral: "#3d4451", // Neutral background
+          "base-100": "#ffffff", // Background for light mode
+          "base-200": "#f9fafb", // Paper background for light mode
+          "base-content": "#1f2937", // Text color for light mode
+        },
+      },
+    ],
+  },
   plugins: [daisyui],
 };
 
