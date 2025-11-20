64c81e4a6b22d5179f6b081a61855c66b715493a
olivier-bernard <olivier_bernard@ymail.com>
2025-11-20 22:11:58 +0000

    nitofocation management improvements and ui improvements for notification

--- src/app/components/LogoutButton.tsx
+++ src/app/components/LogoutButton.tsx
@@ -4,12 +4,15 @@ import { signOut } from "next-auth/react";
 import { getTranslations } from "../lib/i18n";
 
 // This is a client component because it uses the onClick event handler
-export default function LogoutButton({ lang }: { lang: string }) {
+export default function LogoutButton({ lang, onClick }: { lang: string, onClick?: () => void }) {
   const { navBar } = getTranslations(lang);
 
   return (
-    <li>
-      <a className="text-base" onClick={() => signOut({ callbackUrl: '/login' })}>{navBar.logout}</a>
-    </li>
+    <a
+      className="text-base cursor-pointer block px-4 py-2 hover:bg-base-200"
+      onClick={() => signOut({ callbackUrl: '/login' })}
+    >
+      {navBar.logout}
+    </a>
   );
 }
